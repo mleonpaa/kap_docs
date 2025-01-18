a9bcf65666b33c402fc5f24742a3d2a7a69591ff
Marcos León Paavilainen <marcos.leon098@gmail.com>
2025-01-18 23:16:30 +0100

    Create s3_deploy.tf

---
+++ s3_deploy.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "k8s_storage"{
+    bucket = "kap-bucket"
+    force_destroy = true
+    tags = {
+        Name = "kap-bucket"
+    }
+
+}
